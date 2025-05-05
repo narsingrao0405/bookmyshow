e0747f88fceacb081823eca3e74618671b10a84a
Narsing Rao <narsing0405@gmail.com>
2025-05-05 13:20:20 +0530

    feat: implement login and registration forms with validation

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React from 'react';
+import { Form, Button, Input } from "antd";
+import {Link} from "react-router-dom";
 
-function Login()  {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
+function Login() {
+
+    const onFinish = (values: any) => {
+        console.log("SUbmitted Values are ", values);
+    }
+    return (
+        <>
+
+        <main className='App-header'>
+            <h1>Login to BookMyShow</h1>
+            <section className='mw-500 text-center px-3"'>
+
+                <Form layout='vertical' onFinish={onFinish}>
+                    <Form.Item
+                        label='Email/User Name'
+                        name='email'
+                        className='d-block'
+                        htmlFor='email'
+                        rules={[
+                            { required: true, message: 'Please input your email!' },
+                            { type: 'email', message: 'Please input a valid email!' }
+                        ]}
+                    >
+                        <Input
+                            id='email'
+                            type='text'
+                            placeholder='email'
+                        />
+
+                    </Form.Item>
+                    <Form.Item
+                        label='Password'
+                        name='password'
+                        className='d-block'
+                        htmlFor='password'
+                        rules={[
+                            { required: true, message: 'Please input your password!' }
+
+                        ]}
+                    >
+                        <Input
+                            placeholder='password'
+                            type='password'
+                            id='password'
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        className='d-block'>
+                        <Button
+                            type='primary'
+                            htmlType='submit'
+
+                            style={{ fontSize: '1.5rem', fontWeight: '600' }}
+                            block
+                        >
+                            Login
+
+                        </Button>
+                    </Form.Item>
+                    <div>
+                        <p className='text-center'>
+                            Don't have an account? <Link to="/register">Register</Link>
+                        </p>
+                    </div>
+                </Form>
+            </section>
+        </main>
+
+    </>
+
+    )
 };
 
 export default Login;
--- client/src/pages/Register/index.tsx
+++ client/src/pages/Register/index.tsx
@@ -1,11 +1,85 @@
 import React from 'react';
 
+import {Form, Input, Button} from "antd";
+import {Link} from "react-router-dom";
+
 function Register() {
+    const onFinish = (values: any) => {
+        console.log("Submitted Values are ", values);
+    }
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <>
+    <main className='App-header'>
+        <h1> Register to BookMyShow</h1>
+        <section className='mw-500 text-center px-3"'>
+            <Form layout='vertical' onFinish={onFinish}>
+                <Form.Item
+                    label="Full Name"
+                    name="name"
+                    className='d-block'
+                    htmlFor='name'
+                    rules={[
+                        {required: true, message: 'Please input your Name!'},
+                    ]}>
+                    <Input
+                        id='name'
+                        type='text'
+                        placeholder='Name'
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    className='d-block'
+                    htmlFor='email'
+                    rules={[
+                        {required: true, message: 'Please input your email!'},
+                        {type: 'email', message: 'Please input a valid email!'}
+                    ]}>
+                    <Input
+                        id='email'
+                        type='text'
+                        placeholder='email'
+                    />
+            
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    className='d-block'
+                    htmlFor='pas
+                    '
+                    rules={[
+                        {required: true, message: 'Please input your Password!'},
+                    ]}>
+                    <Input
+                        id='password'
+                        type='password'
+                        placeholder='password'
+                    />
+            
+                </Form.Item>
+                <Form.Item
+                    className='d-block'>
+                    <Button
+                        type='primary'
+                        htmlType='submit'
+                        style={{fontSize: '1.5rem', fontWeight: '600'}}
+                        block
+                    >
+                        Register
+                    </Button>
+                </Form.Item>
+                <div>
+                    <p>Already Have the Account <Link to="/login"> Login </Link></p>
+                </div>
+            </Form>
+
+        </section>
+    </main>
+    </>
   )
 }
 
-export default Register
+export default Register;
